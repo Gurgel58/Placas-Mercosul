0e8719325e3c33a40d335c6a5fb4e64304cbf414
Alexandre Gurgel <alexandre.gurgel@gmail.com>
2025-04-28 12:47:31 -0300

    Arquivos
    Arquivos relativos ao script Placas

---
+++ Arquivos/funcoes_placas.py
@@ -0,0 +1,34 @@
+#Gerador de Placas Padrão Mercosul V.2.0
+#Conforme Resolução Contran 969/2022 Anexo I
+#Funções necessárias
+
+def chamar_menu():
+	escolha = input("Digite:\n<N> Para Nova placa\n<T> para Trocar a placa").upper()
+	return escolha
+
+def placa_nova():
+	for x in range(7):
+		print(x)
+		if x == 3 or x > 4:
+			c1 = str(int(random.random() * 10))
+			else:
+				c1 = chr(int((random.random() * 26) + 65))
+		pl = pl + c1
+		placa = pl[0:3] + "-" + pl[3:]
+	return placa
+
+def troca_placa():
+	troc = {"0": "A", "1": "B", "2": "C", "3":"D", "4": "E",
+			"5": "F", "6": "G", "7": "H","8": "I", "9": "J"}
+	Pat = input("informe a placa atual (sem espaços):").upper()
+	Pat1 = Pat[0:4]; Pat2 = Pat[5:]; Ptr = Pat[4]; Pat3 = troc.pop(Ptr)
+	pl = Pat1 + Pat3 + Pat2
+	placa = pl[0:3] + "-" + pl[3:]
+	return placa
+
+def grava_placas(dia_hoje, placas_dia):
+	dict = [dia_hoje: placas_dia]
+	with open("placas.csv", "a") as plc:
+		for chave, valor in dict.itens():
+			plc.write(chave + ";" + valor + "\n")
+	return "\nPlacas do dia", dia_hoje, "gravadas."
